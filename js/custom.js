b629337d35204cc94bd49f4008a05dc1cd33f428
Christian Winkler <chris@bashit.me>
2018-05-04 23:52:21 +0200

    trdt

--- js/custom.js
+++ js/custom.js
@@ -151,25 +151,11 @@ $(function () {
  
      $('#contact-form').on('submit', function (e) {
          if (!e.isDefaultPrevented()) {
-             var url = "contact.php";
- 
-             $.ajax({
-                 type: "POST",
-                 url: url,
-                 data: $(this).serialize(),
-                 success: function (data)
-                 {
-                     var messageAlert = 'alert-' + data.type;
-                     var messageText = data.message;
- 
-                     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                     if (messageAlert && messageText) {
-                         $('#contact-form').find('.messages').html(alertBox);
-                         $('#contact-form')[0].reset();
-                     }
-                 }
-             });
-             return false;
+            emailjs.send("gmail", "temp", {"reply_to":"x@gmail.com","from_name":"Name","subject":"SUbject","message_html":"Message"});
+            var alertBox = '<div class="alert ' + 'alert-success' + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + 'worked' + '</div>';
+            $('#contact-form').find('.messages').html(alertBox);
+            $('#contact-form')[0].reset();
+            return false;
          }
      });
 
